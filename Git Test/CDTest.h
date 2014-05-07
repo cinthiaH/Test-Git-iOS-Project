662027de504bc01e3357afbf7906875fbbce82f6
Cinthia Davalos <Cinthia_Davalos@MacBook-Pro-Cinthia.local>
2014-05-07 09:51:48 -0500

    added a test class

---
+++ Git Test/CDTest.h
@@ -0,0 +1,13 @@
+//
+//  CDTest.h
+//  Git Test
+//
+//  Created by Cinthia Davalos on 07/05/14.
+//  Copyright (c) 2014 Cinthia Davalos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CDTest : NSObject
+
+@end
---
+++ Git Test/CDTest.m
@@ -0,0 +1,13 @@
+//
+//  CDTest.m
+//  Git Test
+//
+//  Created by Cinthia Davalos on 07/05/14.
+//  Copyright (c) 2014 Cinthia Davalos. All rights reserved.
+//
+
+#import "CDTest.h"
+
+@implementation CDTest
+
+@end
